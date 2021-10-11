d356e9c0736f8c516215c773ec5f202ef49f085a
BerniDV <59065363+BerniDV@users.noreply.github.com>
2021-10-11 16:07:00 +0200

    Creat GameMode

---
+++ Source/NausTFGMP/ActionGameMode.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ActionGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAUSTFGMP_API AActionGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+
+	AActionGameMode();
+	
+};
