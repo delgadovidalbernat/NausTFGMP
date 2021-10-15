a9cc6b1c4828225fdf26c71f89dd89f65ccbfd2a
BerniDV <59065363+BerniDV@users.noreply.github.com>
2021-10-15 15:34:27 +0200

    Revert "Base main menu"

--- Source/NausTFGMP/ActionPlayerController.cpp
+++ Source/NausTFGMP/ActionPlayerController.cpp
@@ -3,19 +3,11 @@
 
 #include "ActionPlayerController.h"
 #include "PilotActionPawn.h"
-#include "Gui/Menu/MainMenu_EP.h"
-#include "Kismet/KismetSystemLibrary.h"
-#include "Kismet/GameplayStatics.h"
 
 AActionPlayerController::AActionPlayerController() {
 
-	//Busca el PilotActionBP y guarda la referencia de la clase
-	InitializePilotPawnClass();
-
-	//Busca el mainMenuBP y guarda la referencia de la clase
-	InitializeMainMenuClass();
-
-	
+	ConstructorHelpers::FClassFinder <APilotActionPawn> refBlueprint(TEXT("/Game/Blueprints/Action/PilotActionPawn_BP"));
+	pilotClass = refBlueprint.Class;
 }
 
 
@@ -23,88 +15,7 @@ void AActionPlayerController::BeginPlay() {
 
 	Super::BeginPlay();
 
-
-	pilot = GetWorld()->SpawnActor<APilotActionPawn>(pilotClass);
+	APilotActionPawn* pilot = GetWorld()->SpawnActor<APilotActionPawn>(pilotClass);
 	Possess(pilot);
-
-	//Crea el widget de tipo mainMenu y lo muestra
-	LoadMainMenu();
-
-	//Binding signals
-	mainMenu->signalOnClickExit.AddDynamic(this, &AActionPlayerController::ExitGame);
-	mainMenu->signalOnClickPlay.AddDynamic(this, &AActionPlayerController::UnloadMainMenu);
-}
-
-void AActionPlayerController::InitializeMainMenuClass()
-{
-	ConstructorHelpers::FClassFinder <UMainMenu_EP> mainMenuClassBP(TEXT("/Game/Gui/Menu/MainMenu_BP"));
-	mainMenuClass = mainMenuClassBP.Class;
-	
-}
-
-void AActionPlayerController::InitializePilotPawnClass()
-{
-
-	ConstructorHelpers::FClassFinder <APilotActionPawn> pilotClassBP(TEXT("/Game/Blueprints/Action/PilotActionPawn_BP"));
-	pilotClass = pilotClassBP.Class;
-}
-
-void AActionPlayerController::LoadMainMenu()
-{
-
-	//Unicamente queremos que la HUD la tengan los clientes y alimentarla con datos del servidor, en el caso de un solo jugador 
-	//si serà la autoridad por lo tanto si està controlado localmente también mostramos el menu
-	if (pilot->IsLocallyControlled() || !HasAuthority())
-	{
-
-		mainMenu = CreateWidget<UMainMenu_EP>(this, mainMenuClass);
-		mainMenu->bIsFocusable = true;
-		mainMenu->AddToViewport();
-		ShowNotLockingMouseCursor(mainMenu);
-	}
-}
-
-void AActionPlayerController::UnloadMainMenu()
-{
-
-	//Unicamente queremos que la HUD la tengan los clientes y alimentarla con datos del servidor, en el caso de un solo jugador 
-	//si serà la autoridad por lo tanto si està controlado localmente también mostramos el menu
-	if (pilot->IsLocallyControlled() || !HasAuthority())
-	{
-
-		mainMenu->RemoveFromViewport();
-
-		HideAndLockMouseCursor(mainMenu);
-		mainMenu->bIsFocusable = false;
-		
-	}
-}
-
-void AActionPlayerController::ShowNotLockingMouseCursor(UUserWidget* UIMenu)
-{
-
-	FInputModeUIOnly InputModeData;
-	InputModeData.SetWidgetToFocus(UIMenu->TakeWidget());
-	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
-
-	SetInputMode(InputModeData);
-	SetShowMouseCursor(true);
-}
-
-void AActionPlayerController::HideAndLockMouseCursor(UUserWidget* UIMenu)
-{
-
-	FInputModeGameOnly InputModeData;
-
-	//UGameplayStatics::SetViewportMouseCaptureMode(GetWorld(), EMouseCaptureMode::NoCapture);
-
-	SetShowMouseCursor(false);
-	SetInputMode(InputModeData);
-}
-
-void AActionPlayerController::ExitGame()
-{
-	
-	UKismetSystemLibrary::QuitGame(GetWorld(), this, EQuitPreference::Quit, false);
 
 }
--- Source/NausTFGMP/ActionPlayerController.h
+++ Source/NausTFGMP/ActionPlayerController.h
@@ -7,7 +7,6 @@
 #include "ActionPlayerController.generated.h"
 
 class APilotActionPawn;
-class UMainMenu_EP;
 
 /**
  * 
@@ -23,28 +22,9 @@ public:
 
 	virtual void BeginPlay() override;
 
-	void InitializeMainMenuClass();
-
-	void InitializePilotPawnClass();
-
-	void LoadMainMenu();
-
-	UFUNCTION()
-	void UnloadMainMenu();
-
-	void ShowNotLockingMouseCursor(UUserWidget* UIMenu);
-
-	void HideAndLockMouseCursor(UUserWidget* UIMenu);
-
-	UFUNCTION()
-	void ExitGame();
-
 
 private:
 
 	TSubclassOf<APilotActionPawn> pilotClass;
-	APilotActionPawn* pilot;
 	
-	TSubclassOf<UMainMenu_EP> mainMenuClass;
-	UMainMenu_EP* mainMenu;
 };
