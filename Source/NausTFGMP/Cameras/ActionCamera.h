f764ed554f2afac39cfbb163f3a7255868165f30
BerniDV <59065363+BerniDV@users.noreply.github.com>
2021-10-22 11:17:58 +0200

    crea clase base per camera

---
+++ Source/NausTFGMP/Cameras/ActionCamera.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "ActionCamera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NAUSTFGMP_API AActionCamera : public ACameraActor
+{
+	GENERATED_BODY()
+	
+};
